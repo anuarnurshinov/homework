4910bc3024db8d7f437feb9b87ba83f571469cd0
Anuar Nurshinov <anuarnurshinov@gmaul.com>
2022-06-01 18:25:06 +0300

    Урок 4

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?").replace(/[\s.,%]/g, ''))
+let title = prompt("Как называется ваш проект?", 'КальКуляТор');
+let screens = prompt("Какие типы экранов нужно разработать?", "Простой, сложный");
+let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?", "1000 рублей").replace(/[\s.,%]/g, ''))
 let rollback = 15;
 let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice1 = parseInt(prompt("Сколько это будет стоить?").replace(/[\s.,%]/g, ''))
-let service2 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice2 = parseInt(prompt("Сколько это будет стоить?").replace(/[\s.,%]/g, ''))
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Создание слайдера')
+let servicePrice1 = parseInt(prompt("Сколько это будет стоить?", "1000 рублей").replace(/[\s.,%]/g, ''))
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Создание слайдера')
+let servicePrice2 = parseInt(prompt("Сколько это будет стоить?", "1000 рублей").replace(/[\s.,%]/g, ''))
+let allServicePrices = function getAllServicePrices() {
+    return servicePrice1 + servicePrice2
+}
+let fullPrice
+function getFullPrice(params) {
+    fullPrice = parseInt(fullPrice)
+    fullPrice = screenPrice + allServicePrices()
+}
+getFullPrice(fullPrice)
+function getTitle(title) {
+    title.replace(/\s/g, '');
+    return title[0].toUpperCase() + title.slice(1).toLowerCase()
+}
+servicePercentPrice = function getServicePercentPrices() {
+    return Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
+}
+console.log(servicePercentPrice())
 
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-
-
-servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
-console.log(servicePercentPrice)
 
 switch (true) {
     case (fullPrice >= 30000):
@@ -32,11 +43,12 @@ switch (true) {
         break;
 }
 
-// console.log(typeof title);
-// console.log(typeof fullPrice);
-// console.log(typeof adaptive);
-// console.log(screens.length);
+
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens);
+console.log('Процент отката посреднику за работу - ' + servicePercentPrice())
 // console.log('Стоимость верстки экранов ', screenPrice, ' рублей');
 // console.log('Стоимость разработки сайта', fullPrice, ' рублей');
-// console.log(screens.toLowerCase().split(", "));
-// console.log('Процент отката посреднику за работу', fullPrice * (rollback / 100));
